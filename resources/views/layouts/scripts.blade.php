a13a9c566ca9677bbe62b4fa711767e6f6d05200
PhamNhai <nhaipham3295@gmail.com>
2017-08-16 11:44:09 +0700

    gj

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,11 +1,11 @@
-<script src={{ asset('assets/jquery/dist/jquery.min.js')}}></script>
-<script src={{ asset('assets/bootstrap/dist/js/bootstrap.min.js') }}></script>
+<script src={{ asset('/assets/jquery/dist/jquery.min.js')}}></script>
+<script src={{ asset('/assets/bootstrap/dist/js/bootstrap.min.js') }}></script>
 <script src="/js/app.js"></script>
-<script src={{ asset('js/appjs.js')}}></script>
-<script src={{ asset('js/plugins.js')}}></script>
-{!! Html::script('js/sb-admin-2.js') !!}
-{!! Html::script('assets/metisMenu/dist/metisMenu.min.js') !!}
-{!! Html::script('assets/DataTables/media/js/jquery.dataTables.min.js') !!}
-{!! Html::script('assets/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js') !!}
-<script type="text/javascript" src={{asset('assets/bootstrap-star-rating/js/star-rating.min.js')}}></script>
-<script src={{ asset('js/comment.js')}}></script>
+<script src={{ asset('/js/appjs.js')}}></script>
+<script src={{ asset('/js/plugins.js')}}></script>
+{!! Html::script('/js/sb-admin-2.js') !!}
+{!! Html::script('/assets/metisMenu/dist/metisMenu.min.js') !!}
+{!! Html::script('/assets/DataTables/media/js/jquery.dataTables.min.js') !!}
+{!! Html::script('/assets/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js') !!}
+<script type="text/javascript" src={{asset('/assets/bootstrap-star-rating/js/star-rating.min.js')}}></script>
+<script src={{ asset('/js/comment.js')}}></script>
