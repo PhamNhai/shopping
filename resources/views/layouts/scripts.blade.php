15ead537b8032e644659fab5467f8b6c4108bfc5
PhamNhai <nhaipham3295@gmail.com>
2017-08-16 11:24:06 +0700

    ertyuih

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -3,9 +3,9 @@
 <script src="/js/app.js"></script>
 <script src={{ asset('js/appjs.js')}}></script>
 <script src={{ asset('js/plugins.js')}}></script>
-<script src={{ asset('js/sb-admin-2.js')}}></script>
-<script src={{ asset('assets/metisMenu/dist/metisMenu.min.js') }}></script>
-<script src={{ asset('assets/DataTables/media/js/jquery.dataTables.min.js') }}></script>
-<script src={{ asset('assets/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js') }}></script>
+{!! Html::script('js/sb-admin-2.js') !!}
+{!! Html::script('assets/metisMenu/dist/metisMenu.min.js') !!}
+{!! Html::script(''assets/DataTables/media/js/jquery.dataTables.min.js'') !!}
+{!! Html::script('assets/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js's) !!}
 <script type="text/javascript" src={{asset('assets/bootstrap-star-rating/js/star-rating.min.js')}}></script>
 <script src={{ asset('js/comment.js')}}></script>
