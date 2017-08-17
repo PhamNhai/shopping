6a09480ff84c0908a1aeee12338295027079a3d8
PhamNhai <nhaipham3295@gmail.com>
2017-08-17 08:57:41 +0700

    d1

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,20 +1,19 @@
 
-{!! Html::style('/assets/bootstrap-star-rating/css/theme-krajee-fa.min.css') !!}
-{!! Html::style('/assets/bootstrap/dist/css/bootstrap.min.css') !!}
-{!! Html::style('/css/profile.css') !!}
-{!! Html::style('/css/plugins.css') !!}
-{!! Html::style('/css/main.css') !!}
-{!! Html::style('/css/themes.css') !!}
-{!! Html::style('/assets/font-awesome/css/font-awesome.css') !!}
-{!! Html::style('/assets/bootstrap-star-rating/css/star-rating.css') !!}
-{!! Html::style('/assets/bootstrap-star-rating/css/theme-krajee-fa.css') !!}
-{!! Html::style('/assets/bootstrap-star-rating/css/theme-krajee-svg.css') !!}
-{!! Html::style('/assets/bootstrap-star-rating/css/theme-krajee-uni.css') !!}
-{!! Html::style('/css/product.css') !!}
-{!! Html::style('/css/sb-admin-2.css') !!}
-
-<link href={{ asset("/assets/metisMenu/dist/metisMenu.min.css") }} rel="stylesheet">
-<link href={{ asset("/assets/font-awesome/css/font-awesome.min.css") }} rel="stylesheet">
-<link href={{ asset("/assets/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css") }} rel="stylesheet">
-<link href={{ asset("/assets/datatables-responsive/css/responsive.dataTables.scss") }} rel="stylesheet">
+{!! Html::style('assets/bootstrap-star-rating/css/theme-krajee-fa.min.css') !!}
+{!! Html::style('assets/bootstrap/dist/css/bootstrap.min.css') !!}
+{!! Html::style('css/profile.css') !!}
+{!! Html::style('css/plugins.css') !!}
+{!! Html::style('css/main.css') !!}
+{!! Html::style('css/themes.css') !!}
+{!! Html::style('assets/font-awesome/css/font-awesome.css') !!}
+{!! Html::style('assets/bootstrap-star-rating/css/star-rating.css') !!}
+{!! Html::style('assets/bootstrap-star-rating/css/theme-krajee-fa.css') !!}
+{!! Html::style('assets/bootstrap-star-rating/css/theme-krajee-svg.css') !!}
+{!! Html::style('assets/bootstrap-star-rating/css/theme-krajee-uni.css') !!}
+{!! Html::style('css/product.css') !!}
+{!! Html::style('css/sb-admin-2.css') !!}
+{!! Html::style('assets/datatables-responsive/css/responsive.dataTables.scss')}
+<link href={{ asset("assets/metisMenu/dist/metisMenu.min.css") }} rel="stylesheet">
+<link href={{ asset("assets/font-awesome/css/font-awesome.min.css") }} rel="stylesheet">
+<link href={{ asset("assets/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css") }} rel="stylesheet">
 
